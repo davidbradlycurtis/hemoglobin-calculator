b063ff561f94052cf3abd2a4e8fcb82b6025b674
davidbradlycurtis <s531274@nwmissouri.edu>
2020-11-17 20:59:15 -0600

    Added input checking

--- custom-elements/calc-header.js
+++ custom-elements/calc-header.js
@@ -11,30 +11,6 @@ class calcHeader extends HTMLElement {
       <a rel="noopener" class="navbar-brand"
         href="#">${this.getAttribute('shortNum')}</a>
   
-      <!-- Collapse button -->
-      <button class="navbar-toggler" type="button"
-        data-toggle="collapse" data-target="#navbarCollapse"
-        aria-controls="navbarCollapse" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-  
-      <!-- Collapsible content -->
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-  
-        <!-- Left Links -->
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a rel="noopener" class="nav-link" href="#">Calculator <span
-                class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a rel="noopener" class="nav-link"
-              href=${this.getAttribute('siteURL')} target="_blank">About</a>
-          </li>
-        </ul>
-      </div>
     </nav>
       `;
     }
--- scripts/calculatehemoglobin.js
+++ scripts/calculatehemoglobin.js
@@ -5,7 +5,6 @@
 // copyright (c) 2020
 // all rights reserved.
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 function calculatehemoglobin() {
 
   document.getElementById("result").value = ""; 
@@ -15,36 +14,14 @@ function calculatehemoglobin() {
   var fluid = document.getElementById("fluid").value;
   var bloodVolume;
 
-  //Blood Volume
   if (male.checked) {
     bloodVolume = (weight * 75) / 100;
   }
   else if (female.checked) {
     bloodVolume = (weight * 65) / 100;
   }
-  else {
-    alert("Please select a sex.");
-    return;
-  }
 
   var result = ((hemoglobin * bloodVolume) / ((bloodVolume / 10) + fluid * .25)) / 10;
 
   document.getElementById("result").value = String(Math.round((result + Number.EPSILON) * 100) / 100) + " g/dL";
-}
-
-function validate() {
-
-    var invalidClassName = 'invalid'
-    var inputs = document.querySelectorAll('input, select, textarea')
-    inputs.forEach(function (input) {
-      input.addEventListener('invalid', function () {
-      input.classList.add(invalidClassName)
-
-      input.addEventListener('input', function () {
-        if (input.validity.valid) {
-          input.classList.remove(invalidClassName)
-        }
-      })
-      })
-    })
 }
